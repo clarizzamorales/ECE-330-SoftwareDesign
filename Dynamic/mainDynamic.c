09f9c37d212ae7c5ce5f309e9744c22cff93c5b3
clarizzamorales <clarizza@MacBook-Pro.local>
2021-02-14 04:12:49 -0700

    Updating

---
+++ Dynamic/mainDynamic.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix_dynamic.h"
+
+int main()
+{
+ int rows;
+ int colms;
+
+ printf("Hi, please enter # of rows and # of columns 1 at a time:\n");
+ scanf("%d %d", &rows, &colms);
+
+//Create an empty matrix
+	matrix mat;
+	matrix transposeOfMat;
+	mat = create_initval(rows, colms, 0);
+
+	//Store matrix elements by asking user to enter them
+	
+	int a;
+	int b;
+	double val;
+
+	printf("Please, input the elements for your matrix:\n");
+	for(a=0; a < rows; a++){
+		for(b=0; b < colms; b++){
+			printf("(Row[%d],Col[%d]):\n", a, b);
+			scanf("%lf", &val);
+			assign(a,b,&mat, val);
+		}
+	}
+	
+	//print out matrix entered by the user
+	
+	printf("You have entered the following matrix:\n");
+
+	matrix_print(mat);
+
+	//take the transpose of the matrix 
+	transposeOfMat = TransposeOfMatrix(mat);
+	
+	//print out output of transposed matrix
+	
+	printf("Your transposed matrix is: ");
+	matrix_print(transposeOfMat);
+
+
+}
--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
-run_staticMat: main.o matrix_static.o
-	gcc main.o matrix_static.o -o run_staticMat
+run_staticMat: main.o matrix_static.o mainDynamic.o matrix_dynamic.o
+	gcc main.o matrix_static.o mainDynamic.o matrix_dynamic.o -o run_staticMat
 
 main.o: main.c matrix_static.h
 	gcc -c main.c
 
 matrix_static.o: matrix_static.c  matrix_static.h
 	gcc -c matrix_static.c
+
+mainDynamic.o: mainDynamic.c matrix_dynamic.h
+	gcc -c mainDynamic.c
+
+matrix_dynamic.p: matrix_dynamic.c matrix_dynamic.h
+	gcc -c matrix_dynamic.c
 
 clean:
 	rm *.o
